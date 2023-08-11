8148f4cabc13462871320d2e47bf9e9e4c298f12
grmbyrn <graemebyrne_89@hotmail.com>
2023-08-11 16:45:14 +0200

    add store, add data to store and reconfigure components so each has access to that data

--- src/components/FeedbackList.svelte
+++ src/components/FeedbackList.svelte
@@ -1,9 +1,23 @@
 <script>
+    import { onMount, onDestroy } from 'svelte';
+    import {FeedbackStore} from '../stores'
+    import {fade, scale} from 'svelte/transition'
     import FeedbackItem from "./FeedbackItem.svelte";
+    // let feedback = []
 
-    export let feedback = []
+    // const unsubscribe = FeedbackStore.subscribe((data) => feedback = data)
+
+    // onMount(() => {
+    //     console.log('mounted');
+    // })
+
+    // onDestroy(() => {
+    //     unsubscribe();
+    // })
 </script>
 
-{#each feedback as fb (fb.id)}
-    <FeedbackItem item={fb} on:delete-feedback />
+{#each $FeedbackStore as fb (fb.id)}
+    <div in:scale out:fade='{{duration:500}}'>
+        <FeedbackItem item={fb}/>
+    </div>
 {/each}
--- src/components/FeedbackStats.svelte
+++ src/components/FeedbackStats.svelte
@@ -1,12 +1,14 @@
 <script>
-    export let count
-    export let average
+    import {FeedbackStore} from '../stores'
+
+    $: count = $FeedbackStore.length
+	$: average = $FeedbackStore.reduce((acc, c) => acc + c.rating, 0) / count
 </script>
 
 
 <div class="feedback-stats">
     <h4>{count} Reviews</h4>
-    <h4>Ratings Average: {average}</h4>
+    <h4>Ratings Average: {average > 0 ? average : 0}</h4>
 </div>
 
 <style>
